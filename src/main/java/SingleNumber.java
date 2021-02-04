5a469d741a56799c2e5b1b46394ed8617a0cb7bf
linyongjiang <linyongjiang@aspirecn.com>
2021-02-04 18:39:17 +0800

    打卡

---
+++ src/main/java/SingleNumber.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i=i+2) {
+            if(i == nums.length - 1){
+                return nums[i];
+            }
+            if(nums[i] != nums[i + 1]){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
