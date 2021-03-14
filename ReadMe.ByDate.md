727edac955cf78bb1b8723b616a8564c9385c136
riveralan <riveralan.lin@qq.com>
2021-03-14 20:13:13 +0800

    第二周记录

--- ReadMe.ByDate.md
+++ ReadMe.ByDate.md
@@ -11,3 +11,9 @@ difficulty level ：
     - [solution code](solution/FanZhuanLianBiaoLcof.java)
 - :green_heart: [二叉树的层序遍历](https://leetcode-cn.com/problems/binary-tree-level-order-traversal/)
     - [solution code](solution/BinaryTreeLevelOrderTraversal.java)
+
+## Week1-2021年03月14号
+- :green_heart: [112.Path Sum](https://leetcode-cn.com/problems/path-sum/)
+  - [source code](solution/PathSum.java)
+- :green_heart: [1114.Print in Order](https://leetcode-cn.com/problems/print-in-order/)
+  - [source code](solution/PrintInOrder.java)
---
+++ solution/PathSum.java
@@ -0,0 +1,83 @@
+package LeecodeNote.solution;
+
+/**
+ * 给你二叉树的根节点 root 和一个表示目标和的整数 targetSum 。判断该树中是否存在 根节点到叶子节点 的路径，这条路径上所有节点值相加等于目标和
+ * targetSum 。如果存在，返回 true ；否则，返回 false 。
+ * <p>
+ * 叶子节点 是指没有子节点的节点。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+ * 输出：true
+ * 解释：等于目标和的根节点到叶节点路径如上图所示。
+ * <p>
+ * <p>
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：root = [1,2,3], targetSum = 5
+ * 输出：false
+ * 解释：树中存在两条根节点到叶子节点的路径：
+ * (1 --> 2): 和为 3
+ * (1 --> 3): 和为 4
+ * 不存在 sum = 5 的根节点到叶子节点的路径。
+ * <p>
+ * 示例 3：
+ * <p>
+ * <p>
+ * 输入：root = [], targetSum = 0
+ * 输出：false
+ * 解释：由于树是空的，所以不存在根节点到叶子节点的路径。
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * <p>
+ * 树中节点的数目在范围 [0, 5000] 内
+ * -1000 <= Node.val <= 1000
+ * -1000 <= targetSum <= 1000
+ * <p>
+ * <p>
+ * Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 1255 👎 0
+ */
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+class PathSum {
+    public boolean hasPathSum(TreeNode node, int sum) {
+        if(node == null) {
+            return false;
+        }
+        //达到叶节点
+        if(node.left == null && node.right ==null){
+            return node.val == sum ;
+        }
+        return hasPathSum(node.left,sum-node.val) || hasPathSum(node.right,sum-node.val);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
